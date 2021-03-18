035c0b63d740a32bf71c3e05903444bdffe79a3e
Deren B <dbozerprogramming@gmail.com>
2021-03-17 22:03:30 -0400

    Added JS files
    Adding JS files to test database setup

---
+++ Creature.js
@@ -0,0 +1,10 @@
+// class Creature {
+//     constructor(inbound) {
+//         this.inbound = inbound;
+//         alert("This is a cracker of " + inbound);
+//     }
+// }
+
+// function testo(bigo) {
+//     alert("This is a test of " + bigo);
+// }
